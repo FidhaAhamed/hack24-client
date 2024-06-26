f07896d5947e545b88cabff8972483a34874db35
Adwaith Raj <adwaithraj45654@gmail.com>
2024-06-26 17:25:19 +0530

    chngs

--- src/components/about.tsx
+++ src/components/about.tsx
@@ -1,8 +1,30 @@
+import Heading from "./heading";
+
 export default function About() {
   return (
     <div className="w-dvw h-dvh bg-about">
       <div className="z-10 absolute w-full h-full flex flex-col md:grid md:grid-cols-2 p-8">
-        <div className="col-span-1"></div>
+        <div className="col-span-1 w-full h-full flex flex-col place-content-center place-items-center lg:p-20">
+          <Heading heading="About" subheading=".hack(); 2024" />
+          <div className="text-center flex flex-col gap-y-4 text-snow">
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum
+              pariatur, facere sequi culpa nulla magnam, nobis aliquam iste
+              voluptatibus unde nisi dolore iure? Consectetur harum id, eius ex
+              sed deleniti perspiciatis cumque mollitia ipsa! Ut eum architecto
+              voluptatum veritatis explicabo nobis quas, nisi quis
+              necessitatibus vitae ab velit minus autem corporis consequuntur
+              enim? Tenetur fugit doloremque inventore voluptatum recusandae
+              veritatis.
+            </p>
+            <p>
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Libero
+              obcaecati ea voluptatum quis impedit esse odio? Ad, similique odit
+              facilis in, hic dicta assumenda sunt accusamus iusto provident
+              culpa excepturi.
+            </p>
+          </div>
+        </div>
         <div className="col-span-1">hi</div>
       </div>
     </div>
--- src/components/heading.tsx
+++ src/components/heading.tsx
@@ -4,5 +4,12 @@ interface HeadingProps {
 }
 
 export default function Heading(props: HeadingProps) {
-  return <div>heading{props.heading}</div>;
+  return (
+    <div className="text-center p-4 mb-4">
+      <div className=" text-lg text-lava p-0 m-0">{props.heading}</div>
+      <div className=" text-4xl leading-7 text-snow">
+        <b>{props.subheading}</b>
+      </div>
+    </div>
+  );
 }
