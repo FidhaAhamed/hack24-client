fb8edc899f934dc9f4a968870d6533e1c6fbbccd
Alan Antony <voa.alan@gmail.com>
2024-06-27 16:03:26 +0530

    faq

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import "./App.css";
 import About from "./components/about";
+import Faq from "./components/faq";
 
 function App() {
   return (
     <>
       <About></About>
+      <Faq></Faq>
+
     </>
   );
 }
