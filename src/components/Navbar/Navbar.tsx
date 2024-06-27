ec25423087a50d46bc500528934bf193969e8547
FidhaAhamed <fidhaahamedpp@gmail.com>
2024-06-27 20:54:36 +0530

    navbar responsive

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,16 +1,24 @@
+import { useState} from "react";
 import React from "react"
 import "./Navbar.css"
 import HackLogo from "../../assets/hackLogo.png";
-
+import { FaBars, FaTimes} from "react-icons/fa";
 
 const Navbar: React.FC<{}> = () => {
+
+  const [isMobile, setIsMobile] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMobile(!isMobile);
+  };
+
   return (
     <div className="navbar">
       <div className="logo">
         <img className="hack-logo" src= {HackLogo} alt=".hack();_" />
       </div>
 
-      <div className="nav-components">
+      <div className={`nav-components ${isMobile ? "open" : ""}`}>
           <ul>
             <li><a href="">About</a></li>
             <li><a href="">Timeline</a></li>
@@ -21,9 +29,14 @@ const Navbar: React.FC<{}> = () => {
           </ul>
       </div>
 
-      <div className="register-btn">
+      <div className={`register-btn ${isMobile ? "open" : ""}`}>
         <button className="btn">Register</button>
       </div>
+
+      <div className="menu-icon" onClick={toggleMenu}>
+        {isMobile ? <FaTimes /> : <FaBars />}
+      </div>
+
     </div>
   )
 }
