ae949d5f3eb7b397b16dedc1f72da899d8c5b2a8
Alan Antony <voa.alan@gmail.com>
2024-06-28 20:43:34 +0530

    hero and schedule

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,22 @@
 import "./App.css";
 import About from "./components/about";
-import Faq from "./components/faq";
-import Navbar from "./components/Navbar/Navbar"
-import Contact from "./components/Contact/Contact"
-import Footer from "./components/Footer/Footer"
-
+import Faq from "./components/faq/faq";
+import Schedule from "./components/schedule/schedule";
+import Navbar from "./components/Navbar/Navbar";
+import Contact from "./components/Contact/Contact";
+import Footer from "./components/Footer/Footer";
+import Hero from "./components/hero/hero";
 
 function App() {
   return (
     <>
       <Navbar></Navbar>
+      <Hero></Hero>
       <About></About>
+      <Schedule></Schedule>
       <Faq></Faq>
       <Contact></Contact>
       <Footer></Footer>
-
     </>
   );
 }
---
+++ src/components/schedule/schedule.tsx
@@ -0,0 +1,47 @@
+import "./schedule.css";
+import { useState, useEffect } from "react";
+
+export default function Schedule() {
+  const [imageSrc, setImageSrc] = useState<string>("src/assets/line2mob.svg");
+
+  const updateImageSrc = (): void => {
+    const screenWidth = window.innerWidth;
+
+    if (screenWidth >= 900) {
+      setImageSrc("src/assets/Line 2.svg");
+    } else if (screenWidth >= 768) {
+      setImageSrc("src/assets/line2mob.svg");
+    } else {
+      setImageSrc("src/assets/line2mob.svg");
+    }
+  };
+
+  useEffect(() => {
+    // Update image source on initial load
+    updateImageSrc();
+
+    // Update image source on window resize
+    window.addEventListener("resize", updateImageSrc);
+
+    // Cleanup event listener on component unmount
+    return () => window.removeEventListener("resize", updateImageSrc);
+  }, []);
+  return (
+    <section id="timeline">
+      <div className="timeline-container">
+        <div className="upper-heading">Timeline</div>
+        <div className="lower-heading">
+          <h2>Take a Trek Through Our Schedule</h2>
+        </div>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque odio
+          dicta aut esse quis. Nihil, aspernatur ex aliquam esse consequatur
+          corporis debitis, modi accusantium vero et deserunt repellat at
+          molestias!
+        </p>
+      </div>
+
+      <img src={imageSrc} alt="Responsive" />
+    </section>
+  );
+}
