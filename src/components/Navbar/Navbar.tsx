8267a95f962422543731a1e5ce3ebf761d2596c3
Adwaith Raj <adwaithraj45654@gmail.com>
2024-06-29 16:50:50 +0530

    chngs

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,11 +1,9 @@
-import { useState} from "react";
-import React from "react"
-import "./Navbar.css"
+import { useState } from "react";
+import "./Navbar.css";
 import HackLogo from "../../assets/hackLogo.png";
-import { FaBars, FaTimes} from "react-icons/fa";
+import { FaBars, FaTimes } from "react-icons/fa";
 
-const Navbar: React.FC<{}> = () => {
-
+const Navbar = () => {
   const [isMobile, setIsMobile] = useState(false);
 
   const toggleMenu = () => {
@@ -13,20 +11,32 @@ const Navbar: React.FC<{}> = () => {
   };
 
   return (
-    <div className="navbar">
+    <div className={`navbar ${isMobile ? "align" : ""}`}>
       <div className="logo">
-        <img className="hack-logo" src= {HackLogo} alt=".hack();_" />
+        <img className="hack-logo" src={HackLogo} alt=".hack();_" />
       </div>
 
       <div className={`nav-components ${isMobile ? "open" : ""}`}>
-          <ul>
-            <li><a href="">About</a></li>
-            <li><a href="">Timeline</a></li>
-            <li><a href="">Prize Pool</a></li>
-            <li><a href="">Sponsers</a></li>
-            <li><a href="">FAQ</a></li>
-            <li><a href="">Contact</a></li>
-          </ul>
+        <ul>
+          <li>
+            <a href="">About</a>
+          </li>
+          <li>
+            <a href="">Timeline</a>
+          </li>
+          <li>
+            <a href="">Prize Pool</a>
+          </li>
+          <li>
+            <a href="">Sponsers</a>
+          </li>
+          <li>
+            <a href="">FAQ</a>
+          </li>
+          <li>
+            <a href="">Contact</a>
+          </li>
+        </ul>
       </div>
 
       <div className={`register-btn ${isMobile ? "open" : ""}`}>
@@ -36,9 +46,8 @@ const Navbar: React.FC<{}> = () => {
       <div className="menu-icon" onClick={toggleMenu}>
         {isMobile ? <FaTimes /> : <FaBars />}
       </div>
-
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
