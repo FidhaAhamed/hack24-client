8b4fbff0e570aea284bdfa6470157ab4dbf8b523
Adwaith Raj <adwaithraj45654@gmail.com>
2024-06-30 23:57:50 +0530

    some more fixes

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,33 +1,63 @@
-import "./Footer.css"
-import { FaFacebookF, FaWhatsapp, FaLinkedin, FaInstagram, FaTwitter  } from "react-icons/fa";
+import "./Footer.css";
+import {
+  FaFacebookF,
+  FaWhatsapp,
+  FaLinkedin,
+  FaInstagram,
+  FaTwitter,
+} from "react-icons/fa";
 
 function Footer() {
   return (
-    <div className="footer">
-      <div className="footer-details">
-        <div className="footer-about">
-          <h1>About Us</h1>
-          <p>IEEE MACE SB is a non profit organization working for the upliftment of students and professionals.</p>
-          <div className="icons">
-            <a href="#"><FaFacebookF /></a>
-            <a href="#"><FaLinkedin /></a>
-            <a href="#"><FaInstagram /></a>
-            <a href="#"><FaTwitter /></a>
+    <div className="flex flex-col">
+      <div className="footer">
+        <div className="footer-details">
+          <div className="footer-about">
+            <h1>About Us</h1>
+            <p>
+              IEEE MACE SB is a non profit organization working for the
+              upliftment of students and professionals.
+            </p>
+            <div className="icons">
+              <a href="#">
+                <FaFacebookF />
+              </a>
+              <a href="#">
+                <FaLinkedin />
+              </a>
+              <a href="#">
+                <FaInstagram />
+              </a>
+              <a href="#">
+                <FaTwitter />
+              </a>
+            </div>
+          </div>
+          <div className="footer-contact">
+            <h1>Contact Us</h1>
+            <p>
+              <a href="#">
+                <FaWhatsapp /> <span>Name1</span>
+              </a>
+            </p>
+            <p>
+              <a href="#">
+                <FaWhatsapp />
+                <span>Name2</span>
+              </a>
+            </p>
+            <p className="email">
+              <a href="">Email: hack24@mail.com</a>
+            </p>
           </div>
         </div>
-        <div className="footer-contact">
-          <h1>Contact Us</h1>
-          <p><a href="#"><FaWhatsapp /> <span>Name1</span></a></p>
-          <p><a href="#"><FaWhatsapp /><span>Name2</span></a></p>
-          <p className="email"><a href="">Email: hack24@mail.com</a></p>
-        </div>
       </div>
-      <div className="footer-copyright">
+      <div className="footer-copyright bg-orange-600 py-4">
         <p>2024 &copy; .hack24. All Rights Reserved.</p>
         <p>Designed and Developed By .hack24 WebTeam</p>
       </div>
     </div>
-  )
+  );
 }
 
-export default Footer
+export default Footer;
--- src/components/about.tsx
+++ src/components/about.tsx
@@ -1,11 +1,31 @@
 import Heading from "./heading";
 import "./about.scss";
+import { useEffect, useState } from "react";
+
+const useScreenWidth = () => {
+  const [isSmallScreen, setIsSmallScreen] = useState(window.innerWidth < 768);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsSmallScreen(window.innerWidth < 768);
+    };
+
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
+  return isSmallScreen;
+};
 
 export default function About() {
+  const isSmallScreen = useScreenWidth();
   return (
     <div className="relative change w-dvw  md:h-dvh bg-about">
-      <div className="z-10 absolute w-full h-full flex flex-col md:grid md:grid-cols-2 p-8">
-        <div className="col-span-1 w-full h-full flex flex-col place-content-center place-items-center lg:p-20">
+      <div className="z-10 absolute w-full h-fit  md:h-full flex flex-col md:grid md:grid-cols-2 p-8">
+        <div className="col-span-1 w-full h-full pb-8 md:pb-0 flex flex-col place-content-center place-items-center lg:p-20">
           <Heading heading="About" subheading=".hack(); 2024" />
           <div className="text-center flex flex-col gap-y-4 text-snow">
             <p>
@@ -29,37 +49,88 @@ export default function About() {
         <div className="col-span-1 w-full h-full  place-content-center place-items-center lg:p-8">
           {" "}
           <div className="container">
-            <div className="hex bg-lava">
+            <div className="none hex bg-lava">
               <div className="hex-inner"></div>
-              <div className="text-2xl text-snow font-bold">
+              <div className="title text-2xl text-snow font-bold">
                 Good Health and Wellbeing
+              </div>
+              <div className="desc text-sm text-snow font-thin">
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolor
+                quae nihil repudiandae itaque! Architecto sunt, obcaecati
+                doloremque nulla alias possimus.
               </div>
             </div>
 
-            <div className="hex">
+            <div className="none hex">
               <div className="hex-inner"></div>
               <div className="text-2xl text-lava font-bold">Tracks</div>
             </div>
 
+            {isSmallScreen && (
+              <div className=" hex">
+                <div className="hex-inner"></div>
+                <div className="text-2xl text-lava font-bold">Tracks</div>
+              </div>
+            )}
+            {isSmallScreen && (
+              <div className="hex bg-lava">
+                <div className="hex-inner"></div>
+                <div className="title text-2xl text-snow font-bold">
+                  Good Health and Wellbeing
+                </div>
+                <div className="desc text-sm text-snow font-thin">
+                  Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                  Dolor quae nihil repudiandae itaque! Architecto sunt,
+                  obcaecati doloremque nulla alias possimus.
+                </div>
+              </div>
+            )}
+            <div className=" hex bg-void">
+              <div className="hex-inner"></div>
+              <div className="title text-2xl text-snow font-bold">
+                Good Health and Wellbeing
+              </div>
+              <div className="desc text-sm text-snow font-thin">
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolor
+                quae nihil repudiandae itaque! Architecto sunt, obcaecati
+                doloremque nulla alias possimus.
+              </div>
+            </div>
+            {isSmallScreen && (
+              <div className="hex bg-lava">
+                <div className="hex-inner"></div>
+                <div className="title text-2xl text-snow font-bold">
+                  Good Health and Wellbeing
+                </div>
+                <div className="desc text-sm text-snow font-thin">
+                  Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                  Dolor quae nihil repudiandae itaque! Architecto sunt,
+                  obcaecati doloremque nulla alias possimus.
+                </div>
+              </div>
+            )}
             <div className="hex bg-void">
               <div className="hex-inner"></div>
-              <div className="text-2xl text-snow font-bold">
+              <div className="title text-2xl text-snow font-bold">
                 Good Health and Wellbeing
+              </div>
+              <div className="desc text-sm text-snow font-thin">
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolor
+                quae nihil repudiandae itaque! Architecto sunt, obcaecati
+                doloremque nulla alias possimus.
               </div>
             </div>
 
-            <div className="hex bg-void">
+            <div className="none hex bg-lava">
               <div className="hex-inner"></div>
-              <div className="text-2xl text-snow font-bold">
+
+              <div className="title text-2xl text-snow font-bold">
                 Good Health and Wellbeing
               </div>
-            </div>
-
-            <div className="hex bg-lava">
-              <div className="hex-inner"></div>
-
-              <div className="text-2xl text-snow font-bold">
-                Good Health and Wellbeing
+              <div className="desc text-sm text-snow font-thin">
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolor
+                quae nihil repudiandae itaque! Architecto sunt, obcaecati
+                doloremque nulla alias possimus.
               </div>
             </div>
           </div>
