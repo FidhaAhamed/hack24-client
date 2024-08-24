de34abf372a4e81f69572dc0134fee5437675c30
Alan Antony <122230724+stone-PETER@users.noreply.github.com>
2024-08-24 12:00:10 +0530

    Update sponsor.tsx

--- src/components/sponsors/sponsor.tsx
+++ src/components/sponsors/sponsor.tsx
@@ -1,13 +1,14 @@
 import React from "react";
 
 interface SponsorProps {
+  link: string;
   imgLink: string;
 }
 
-const Sponsor: React.FC<SponsorProps> = ({ imgLink }) => {
+const Sponsor: React.FC<SponsorProps> = ({ link,imgLink }) => {
   return (
     <div className="max-w-[300px] p-4 h-[200px] flex items-center justify-center bg-[#da6919e0] rounded-lg -outline-offset-8 hover:bg-slate-800 duration-500 outline-double outline-slate-200">
-      <img className="w-full p-4" src={imgLink} alt="Sponsor Logo" />
+      <a href={link}><img className="w-full p-4" src={imgLink} alt="Sponsor Logo" /></a>
     </div>
   );
 };
