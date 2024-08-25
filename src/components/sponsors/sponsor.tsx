caa55baf86de2f2e56ff2ae6984013bd28755410
Alan Antony <voa.alan@gmail.com>
2024-08-25 08:56:44 +0530

    link target

--- src/components/sponsors/sponsor.tsx
+++ src/components/sponsors/sponsor.tsx
@@ -5,10 +5,12 @@ interface SponsorProps {
   imgLink: string;
 }
 
-const Sponsor: React.FC<SponsorProps> = ({ link,imgLink }) => {
+const Sponsor: React.FC<SponsorProps> = ({ link, imgLink }) => {
   return (
     <div className="max-w-[300px] p-4 h-[200px] flex items-center justify-center bg-[#da6919e0] rounded-lg -outline-offset-8 hover:bg-slate-800 duration-500 outline-double outline-slate-200">
-      <a href={link}><img className="w-full p-4" src={imgLink} alt="Sponsor Logo" /></a>
+      <a href={link} target="_blank" rel="noopener noreferrer">
+        <img className="w-full p-4" src={imgLink} alt="Sponsor Logo" />
+      </a>
     </div>
   );
 };
